11cf6b93a94db24bbe6af726b13f257b3c912a91
SMNDVC <simon.dovicovic@gmail.com>
2024-04-13 13:15:06 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, request
+import os
+import json
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+
+# Load user data from file
+def load_users():
+    data_dir = os.path.join(os.path.dirname(__file__), 'data') 
+    file_path = os.path.join(data_dir, 'users.json')
+    
+    with open(file_path, 'r') as f:
+        users = json.load(f)
+    return users
+
+# Check if username and password match
+def authenticate(username, password):
+    users = load_users()
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            return True
+    return False
+
+@app.route('/update_json', methods=['POST'])
+def update_json():
+    qlcplusIP = request.json['qlcplusIP']
+    data = {"qlcplusIP": qlcplusIP}
+    json_url = os.path.join(app.static_folder, 'config.json')
+    with open(json_url, 'w') as f:
+        json.dump(data, f)
+    return jsonify(success=True)
+
+@app.route('/config')
+def config():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
+    
+    json_url = os.path.join(app.static_folder, 'config.json')
+    with open(json_url, 'r') as f:
+        data = json.load(f)
+    
+    return render_template('/config.html', qlcplusIP=data['qlcplusIP'])
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/bugs')
+def bugy():
+    return render_template('bugy.html')
+
+@app.route('/tutorials')
+def tutorialy():
+    return render_template('tutorialy.html') 
+
+@app.route('/qlcplus')
+def qlcplus():
+    return render_template('qlcplus.html')
+
+# @app.route('/prezentacie')
+# def osvetlenie_podium():
+#     return render_template('osvetlenie/prezentacie.html')
+
+# @app.route('/strop')
+# def osvetlenie_stropu():
+#     return render_template('osvetlenie/strop.html')
+
+# @app.route('/diskocajovna')
+# def diskocajovna():
+#     return render_template('osvetlenie/diskocajovna.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if authenticate(username, password):
+            session['logged_in'] = True
+            return redirect(url_for('config'))
+        else:
+            error = 'Invalid username or password'
+
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('qlcplus'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
---
+++ static/qlcplus.js
@@ -0,0 +1,406 @@
+var isConnected = false;
+var buttons_1 = document.querySelectorAll('.btn-group-1');
+
+window.onload = function() {
+  fetch('/static/config.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      connectToWebSocket(data.qlcplusIP);
+      setTimeout(function() {
+        var connStatus = document.getElementById('connStatus');
+        if (connStatus) {
+          connStatus.classList.remove('d-none');
+        }
+      }, 150);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  buttons_1.forEach(button => {
+      button.addEventListener('click', function() {
+          // Check if the clicked button is already active
+          if (this.classList.contains('active')) {
+              // If yes, remove the active class to untoggle
+              this.classList.remove('active');
+              this.classList.remove('hover');
+          } else {
+              // Otherwise, remove active class from all buttons
+              buttons_1.forEach(btn => btn.classList.remove('active'));
+              // And add active class to the clicked button
+              this.classList.add('active');
+          }
+      });
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.btn-group-2');
+
+  buttons.forEach(button => {
+      button.addEventListener('click', function() {
+          // Check if the clicked button is already active
+          if (this.classList.contains('active')) {
+              // If yes, remove the active class to untoggle
+              this.classList.remove('active');
+              this.classList.remove('hover')
+          } else {
+              // Otherwise, remove active class from all buttons
+              buttons.forEach(btn => btn.classList.remove('active'));
+              // And add active class to the clicked button
+              this.classList.add('active');
+          }
+      });
+  });
+});
+
+
+
+function updateJsonAndReload() {
+  var qlcplusIP = document.querySelector('.form-control').value;
+  fetch('/update_json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({qlcplusIP: qlcplusIP})
+  }).then(function(response) {
+    window.location.reload();
+  }).catch(function(error) {
+    console.error('Error:', error);
+  });
+}
+
+function requestAPI(cmd) {
+  if (isConnected === true)
+    websocket.send("QLC+API|" + cmd);
+  else
+    alert("You must connect to QLC+ WebSocket first!");
+}
+
+function requestAPIWithParam(cmd, paramObjName) {
+  var obj = document.getElementById(paramObjName);
+  if (obj) {
+    if (isConnected === true)
+      websocket.send("QLC+API|" + cmd + "|" + obj.value);
+    else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function requestAPIWith2Params(cmd, paramObjName, param2ObjName) {
+  var obj1 = document.getElementById(paramObjName);
+  var obj2 = document.getElementById(param2ObjName);
+  if (obj1 && obj2) {
+    if (isConnected === true)
+      websocket.send("QLC+API|" + cmd + "|" + obj1.value + "|" + obj2.value);
+    else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function requestChannelsRange(cmd, uniObjName, addressObjName, rangeObjName) {
+  var uniObj = document.getElementById(uniObjName);
+  var addrObj = document.getElementById(addressObjName);
+  var rangeObj = document.getElementById(rangeObjName);
+  if (uniObj && addrObj && rangeObj) {
+    if (isConnected === true)
+      websocket.send("QLC+API|" + cmd + "|" + uniObj.value + "|" + addrObj.value + "|" + rangeObj.value);
+    else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function setSimpleDeskChannel2(addressObjName, channelValueObjName) {
+  websocket.send("CH|" + addressObjName + "|" + channelValueObjName);
+}
+
+function setSimpleDeskChannel(addressObjName, channelValueObjName) {
+  var addrObj = document.getElementById(addressObjName);
+  var valObj = document.getElementById(channelValueObjName);
+  if (addrObj && valObj) {
+    if (isConnected === true)
+      websocket.send("CH|" + addrObj.value + "|" + valObj.value);
+    else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function vcWidgetSetValue2(wIDObjName, wValueObjName, rangeBarId) {
+  websocket.send(wIDObjName + "|" + wValueObjName);
+  if (rangeBarId) {
+    document.getElementById(rangeBarId).value = wValueObjName / 2.55;
+  }
+}
+
+function vcWidgetSetValue(wIDObjName, wValueObjName) {
+  var wObj = document.getElementById(wIDObjName);
+  var valObj = document.getElementById(wValueObjName);
+  if (wObj && valObj) {
+    if (isConnected === true) {
+      websocket.send(wObj.value + "|" + valObj.value);
+      console.log("Stopped succesfully");
+    }
+    else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function vcCueListControl(clIDObjName, clOpObjName, clStepObjName) {
+  var clObj = document.getElementById(clIDObjName);
+  var opObj = document.getElementById(clOpObjName);
+  var stepObj = document.getElementById(clStepObjName);
+  if (clObj && opObj) {
+    if (isConnected === true) {
+      if (opObj.value === "STEP")
+        websocket.send(clObj.value + "|" + opObj.value + "|" + stepObj.value);
+      else
+        websocket.send(clObj.value + "|" + opObj.value);
+    } else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function vcFrameControl(frIDObjName, frOperation) {
+  var frObj = document.getElementById(frIDObjName);
+  var opObj = document.getElementById(frOperation);
+
+  if (frObj && opObj) {
+    if (isConnected === true) {
+      websocket.send(frObj.value + "|" + opObj.value);
+    } else
+      alert("You must connect to QLC+ WebSocket first!");
+  }
+}
+
+function wsOnOpen(ev) {
+  var connStatus = document.getElementById('connStatus');
+  if (connStatus) {
+    connStatus.innerHTML = "<span class='text-success'>Connected</span>";
+  }
+  isConnected = true;
+  console.log("Connected to ws")
+  getWidgetStatus("9")
+  // getWidgetStatus2("4")
+};
+
+function wsOnClose(ev) {
+  console.log("QLC+ connection lost!", ev);
+
+  var connStatus = document.getElementById('connStatus');
+  if (connStatus) {
+    connStatus.innerHTML = "<span class='text-danger'>Not connected</span>";
+  }
+  isConnected = false;
+}
+
+function wsOnError(ev) {
+  console.log("QLC+ connection error!", ev);
+  
+  var connStatus = document.getElementById('connStatus');
+  if (connStatus) {
+    connStatus.innerHTML = "<span class='text-danger'>Not connected</span>";
+  }
+  isConnected = false;
+}
+
+function wsOnMessage(ev) {
+  var msgParams = ev.data.split('|');
+  if (msgParams[0] === "QLC+API") {
+    if (msgParams[1] === "getFunctionsNumber") {
+      document.getElementById('getFunctionsNumberBox').innerHTML = msgParams[2];
+    } else if (msgParams[1] === "getFunctionsList") {
+      var tableCode = "<table class='apiTable'><tr><th>ID</th><th>Name</th></tr>";
+      for (var i = 2; i < msgParams.length; i += 2) {
+        tableCode += "<tr><td>" + msgParams[i] + "</td><td>" + msgParams[i + 1] + "</td></tr>";
+      }
+      tableCode += "</table>";
+      document.getElementById('getFunctionsListBox').innerHTML = tableCode;
+    } else if (msgParams[1] === "getFunctionType") {
+      document.getElementById('getFunctionTypeBox').innerHTML = msgParams[2];
+    } else if (msgParams[1] === "getFunctionStatus") {
+      document.getElementById('getFunctionStatusBox').innerHTML = msgParams[2];
+    } else if (msgParams[1] === "getWidgetsNumber") {
+      document.getElementById('getWidgetsNumberBox').innerHTML = msgParams[2];
+    } else if (msgParams[1] === "getWidgetsList") {
+      var tableCode = "<table class='apiTable'><tr><th>ID</th><th>Name</th></tr>";
+      for (var i = 2; i < msgParams.length; i += 2) {
+        tableCode += "<tr><td>" + msgParams[i] + "</td><td>" + msgParams[i + 1] + "</td></tr>";
+      }
+      tableCode += "</table>";
+      document.getElementById('getWidgetsListBox').innerHTML = tableCode;
+    } else if (msgParams[1] === "getWidgetType") {
+      document.getElementById('getWidgetTypeBox').innerHTML = msgParams[2];
+    } else if (msgParams[1] === "getWidgetStatus") {
+      var status = msgParams[2];
+      if (status === "PLAY")
+        status = msgParams[2] + "(Step: " + msgParams[3] + ")";
+      var element = document.getElementById('getWidgetStatusBox');
+      if (element) {
+        element.innerHTML = status;
+      }
+    }
+    else if (msgParams[1] === "getChannelsValues") {
+      var tableCode = "<table class='apiTable'><tr><th>Index</th><th>Value</th><th>Type</th></tr>";
+      for (var i = 2; i < msgParams.length; i += 3) {
+        tableCode += "<tr><td>" + msgParams[i] + "</td><td>" + msgParams[i + 1] + "</td><td>" + msgParams[i + 2] + "</td></tr>";
+      }
+      tableCode += "</table>";
+      document.getElementById('requestChannelsRangeBox').innerHTML = tableCode;
+    }
+  }
+}
+
+function connectToWebSocket(host) {
+  var url = 'ws://' + host + '/qlcplusWS';
+  websocket = new WebSocket(url);
+  wshost = "http://" + host;
+
+  
+
+
+
+  websocket.onopen = wsOnOpen;
+  websocket.onclose = wsOnClose;
+  websocket.onerror = wsOnError;
+  websocket.onmessage = wsOnMessage;
+};
+
+function loadProject() {
+  var formAction = wshost + "/loadProject";
+  document.getElementById('lpForm').action = formAction;
+}
+
+function toggleFunction(functionId) {
+  websocket.send("QLC+API|getFunctionStatus|" + functionId);
+
+  websocket.onmessage = function(event) {
+    var message = event.data;
+    if (message.startsWith("QLC+API|getFunctionStatus|")) {
+      var functionStatus = message.split("|")[2];
+      if (functionStatus === "Running") {
+        websocket.send('QLC+API|setFunctionStatus|' + functionId + '|0');
+      } else {
+        websocket.send('QLC+API|setFunctionStatus|' + functionId + '|1');
+      }
+    }
+  };
+}
+
+function turnOffFunctions(functionsList) {
+  for (let i = 0; i < functionsList.length; i++) {
+    websocket.send(`QLC+API|setFunctionStatus|${functionsList[i]}|0`);
+  }
+}
+
+function turnOnFunctions(functionsList) {
+  for (let i = 0; i < functionsList.length; i++) {
+    websocket.send(`QLC+API|setFunctionStatus|${functionsList[i]}|1`);
+  }
+}
+
+function turnOnSoloFunctions(functionsList, functionsList2) {
+  turnOnFunctions(functionsList);
+  turnOffFunctions(functionsList2)
+}
+
+function toggleSoloFunction(param1, param2) {
+  toggleFunction(param1);
+  turnOffFunctions(param2);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var rangeBar = document.getElementById("rangeBarPrezentacie");
+  if (rangeBar) {
+    rangeBar.addEventListener("input", function() {
+      var multipliedValue = rangeBar.value * 2.55;
+      var roundedValue = Math.ceil(multipliedValue);
+      var roundedValueString = roundedValue.toString();
+      vcWidgetSetValue2('4', roundedValueString, 'rangeBarPrezentacie');
+      // buttons_1.classList.remove('active')
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var rangeBar = document.getElementById("rangeBarStrop");
+  if (rangeBar) {
+    rangeBar.addEventListener("input", function() {
+      var multipliedValue = rangeBar.value * 2.55;
+      var roundedValue = Math.ceil(multipliedValue);
+      var roundedValueString = roundedValue.toString();
+      vcWidgetSetValue2('9', roundedValueString, 'rangeBarStrop');
+    });
+  }
+});
+
+function resetSliderSetFunctions(widgetId, onFunctionIds = [], offFunctionIds = []) {
+  vcWidgetSetValue2(widgetId, "0");
+  turnOnFunctions(onFunctionIds);
+  turnOffFunctions(offFunctionIds);
+}
+
+function getWidgetStatus(widgetID) {
+  // WebSocket sending logic
+  websocket.send('QLC+API|getWidgetStatus|' + widgetID);
+
+  // WebSocket message handler
+  websocket.addEventListener('message', function(event) {
+    var msgParams = event.data.split('|');
+    if (msgParams[0] === "QLC+API" && msgParams[1] === "getWidgetStatus") {
+      var status = msgParams[2];
+      if (status === "PLAY") {
+        status = status + "(Step: " + msgParams[3] + ")";
+      }
+      // console.log("Widget Status:", status); 
+      var rangeBarStrop = document.getElementById("rangeBarStrop");
+      if (rangeBarStrop) {
+        processedStatus = Math.ceil(status/2.55);
+        if (processedStatus < 10) {
+          rangeBarStrop.value = 0
+        }
+        else {
+          rangeBarStrop.value = Math.ceil(status/2.55);
+        }
+      } 
+    }
+  });
+}
+
+function getWidgetStatus2(widgetID) {
+  // WebSocket sending logic
+  websocket.send('QLC+API|getWidgetStatus|' + widgetID);
+
+  // WebSocket message handler
+  websocket.addEventListener('message', function(event) {
+    var msgParams = event.data.split('|');
+    if (msgParams[0] === "QLC+API" && msgParams[1] === "getWidgetStatus") {
+      var status = msgParams[2];
+      if (status === "PLAY") {
+        status = status + "(Step: " + msgParams[3] + ")";
+      }
+      // console.log("Widget Status:", status); 
+      var rangeBarPrezentacie = document.getElementById("rangeBarPrezentacie");
+      if (rangeBarPrezentacie) {
+        rangeBarPrezentacie.value = Math.ceil(status/2.55);
+      } 
+    }
+  });
+}
+
+function stopAllFunctions(except) {
+  vcWidgetSetValue2("10", "255", undefined)
+  vcWidgetSetValue2(except, "225", undefined)
+}
+
+function selectValue(value) {
+  document.getElementById('qlcplusIPInput').value = value;
+}
